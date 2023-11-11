857e2bce1c1754b0faa89a5e1e97051b1da12f80
TetianaSolona <94628755+TetianaSolona@users.noreply.github.com>
2023-11-11 17:15:47 +0200

    Modify validate_input() function for all class

--- shape.py
+++ shape.py
@@ -38,15 +38,16 @@ class Square(Shape):
 
 class Rectangle(Shape):
     def __init__(self, top_right_x, top_right_y, bottom_left_x, bottom_left_y):
-        self.validate_input([top_right_x, top_right_y, bottom_left_x, bottom_left_y])
         self.top_right_x = top_right_x
         self.top_right_y = top_right_y
         self.bottom_left_x = bottom_left_x
         self.bottom_left_y = bottom_left_y
-
         self.height = abs(self.top_right_x - self.bottom_left_x)
         self.width = abs(self.top_right_y - self.bottom_left_y)
+        self.validate_input(top_right_x, top_right_y, bottom_left_x, bottom_left_y)
 
+    def validate_input(self, top_right_x, top_right_y, bottom_left_x, bottom_left_y):
+        super().validate_input([top_right_x, top_right_y, bottom_left_x, bottom_left_y])
         if self.height == 0 and self.width == 0:
             raise ValueError("Both height and width should not be zero")
 
@@ -61,13 +62,16 @@ class Rectangle(Shape):
 
 class Circle(Shape):
     def __init__(self, center_x, center_y, radius):
-        if radius == 0:
-            raise ValueError("Radius cannot be zero")
-        self.validate_input([center_x, center_y, radius])
+        self.validate_input(center_x, center_y, radius)
 
         self.center_x = center_x
         self.center_y = center_y
         self.radius = radius
+
+    def validate_input(self, center_x, center_y, radius):
+        super().validate_input([center_x, center_y, radius])
+        if radius == 0:
+            raise ValueError("Radius cannot be zero")
 
     def calculate_perimeter(self):
         result = 2 * math.pi * self.radius
@@ -81,8 +85,6 @@ class Circle(Shape):
 # We can create one more class for another shape, for example triangle
 class Triangle(Shape):
     def __init__(self, point_1_x, point_1_y, point_2_x, point_2_y, point_3_x, point_3_y):
-        self.validate_input([point_1_x, point_1_y, point_2_x, point_2_y, point_3_x, point_3_y])
-
         self.point_1_x = point_1_x
         self.point_1_y = point_1_y
         self.point_2_x = point_2_x
@@ -93,7 +95,10 @@ class Triangle(Shape):
         self.side_1 = math.sqrt((self.point_2_x - self.point_1_x) ** 2 + (self.point_2_y - self.point_1_y) ** 2)
         self.side_2 = math.sqrt((self.point_3_x - self.point_2_x) ** 2 + (self.point_3_y - self.point_2_y) ** 2)
         self.side_3 = math.sqrt((self.point_1_x - self.point_3_x) ** 2 + (self.point_1_y - self.point_3_y) ** 2)
+        self.validate_input(point_1_x, point_1_y, point_2_x, point_2_y, point_3_x, point_3_y)
 
+    def validate_input(self, point_1_x, point_1_y, point_2_x, point_2_y, point_3_x, point_3_y):
+        super().validate_input([point_1_x, point_1_y, point_2_x, point_2_y, point_3_x, point_3_y])
         if self.side_1 + self.side_2 <= self.side_3 or self.side_2 + self.side_3 <= self.side_1 \
                 or self.side_1 + self.side_3 <= self.side_2:
             raise ValueError("Invalid sides for a triangle")
